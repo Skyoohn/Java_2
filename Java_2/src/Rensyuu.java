c3dc06161d0d8f6d7c7e3f0e0441281df7796c1c
DESKTOP-4QLEUQH\wagr <naver@naver.com>
2023-07-10 21:13:13 +0900

    jk

--- Java_2/src/Rensyuu.java
+++ Java_2/src/Rensyuu.java
@@ -1,31 +1,34 @@
-class MyThread1 extends Thread{
 
-	@Override
-	public void run() {
-		System.out.println(getName());
-		for(int i = 0; i < 300; i++) {
-			System.out.print("_");
-		}
-	}
-	}
-class MyThread2 extends Thread{
-
-	@Override
-	public void run() {
-		System.out.println(getName());
-		for(int i = 0; i < 300; i++) {
-			System.out.print("|");
-		}
-	}
-	
-}
 public class Rensyuu {
 	public static void main(String[] args) {
-		String name = Thread.currentThread().getName();
-		MyThread1 m1 = new MyThread1();
-		m1.start();
-		MyThread2 m2 = new MyThread2();
-		m2.start();
+		int month = 3;
 		
+		switch(month) {
+		
+		case 12:
+		case 1:
+		case 2:
+		case 3:
+		System.out.println("겨울입니다");
+		break;
+		
+		case 4:
+		case	5:
+		case	6:
+		System.out.println("봄입니다");
+		break;
+		
+		case 7:
+		case 8:
+		System.out.println("여름입니다");
+		break;
+		
+		case 9 :
+		case 10:
+		case 11 :
+		System.out.println("가을입니다");
+		break;
+		
+		}
 				}
 		}
