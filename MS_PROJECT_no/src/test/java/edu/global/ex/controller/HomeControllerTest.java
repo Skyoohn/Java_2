3e8210e0468e51b603d2bc49ca88bf414f0a4a3b
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-09-21 16:37:01 +0900

    1

---
+++ MS_PROJECT_no/src/test/java/edu/global/ex/controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package edu.global.ex.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
