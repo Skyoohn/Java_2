3a54d608e4acdfcef8db69b6bebb181635e39c14
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-11-06 16:57:28 +0900

    a

---
+++ Java_2/src/test.java
@@ -0,0 +1,14 @@
+public class test {
+
+	public static void main(String[] args) {
+	       for (int i = 0; i < 10; i++) {
+	            System.out.print("BLOCK" + i + " ");
+
+	            for (int j = 0; j < 10; j++) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        }
+	        System.out.println("END");
+	    }
+	}
